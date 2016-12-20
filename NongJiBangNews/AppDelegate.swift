b2518698add78c824493fac04278dbebf8fa8ea6
brian0516 <brian_0419@hotmail.com>
2016-12-20 17:19:07 +0800

    createProject

---
+++ NongJiBangNews/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  NongJiBangNews
+//
+//  Created by shuanglong on 16/12/20.
+//  Copyright © 2016年 shuanglong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
